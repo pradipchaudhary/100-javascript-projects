4e5d1e5934e45641928045e24396ae87e3f5a786
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-05-15 22:28:31 +0545

    Create README.md and Document it

--- 58-Browser Extension/popup/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("clickMeButton").addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            action: "content-script",
-            message: "Hello from popup!",
-        });
-    });
-});
